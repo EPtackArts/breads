3f8b0c081f19d6407cccc6145ef31f46ba6283a6
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-03 19:39:04 -0500

    Helper Methods Part 2 with Bonus finished

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,7 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 //INDEX
-breads.get('/', async(req,res) => {
+breads.get('/', (req,res) => {
   Bread.find()
     .then(foundBreads => {
       res.render('index', {
@@ -21,9 +21,11 @@ breads.get('/new', (req, res) => {
 
 // EDIT
 breads.get('/:id/edit', (req, res) => {
-  res.render('edit', {
-    bread: Bread[req.params.id],
-    index: req.params.id
+  Bread.findById(req.params.id)
+  .then(foundBread => {
+    res.render('edit', {
+      bread: foundBread
+    })
   })
 })
 
@@ -73,6 +75,35 @@ breads.delete('/:id', (req, res)=> {
   Bread.findByIdAndDelete(req.params.id)
   .then(deletedBread => {
     res.status(303).redirect('/breads')
+  })
+})
+
+//Insert Many Bonus
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany([
+    {
+      name: 'Rye',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'French',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'Gluten Free',
+      hasGluten: false,
+      image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    },
+    {
+      name: 'Pumpernickel',
+      hasGluten: true,
+      image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+    }
+  ])
+  .then(createBreads => {
+    res.redirect('/breads')
   })
 })
 
